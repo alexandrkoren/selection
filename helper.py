3e1e5ca357f7f86f0b1021ae7acf447d35e211f2
alexandrkoren <alexandr5koren@gmail.com>
2025-07-08 17:13:25 +0300

    Add relationship ratio

--- helper.py
+++ helper.py
@@ -1,4 +1,50 @@
+import csv
+
+# Чтение данных из csv-файла
+def read_from_csv(filename):
+    result = {}
+    with open(filename, mode='r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            key = row[0]                # Ключ в словаре - id животного
+            values = tuple(row[1:])     # Остальные столбцы как tuple
+            result[key] = values
+    return result
+
 # Ожидаемое EBV
 def expected_ebv(mother_ebv, father_ebv):
     return (mother_ebv + father_ebv)/2
 
+# Собираем всех родственников
+def get_relatives(pedigree, id):
+    relatives = {id: 0}
+
+    def find_relatives(current_id, level):
+        mother_id, father_id = pedigree.get(current_id, (None, None))
+        if mother_id:
+            if mother_id not in relatives:
+                relatives[mother_id] = level
+            elif level < relatives[mother_id]:
+                relatives[mother_id] = level
+            find_relatives(mother_id, level + 1)
+        if father_id:
+            if father_id not in relatives:
+                relatives[father_id] = level
+            elif level < relatives[father_id]:
+                relatives[father_id] = level
+            find_relatives(father_id, level + 1)
+    
+    find_relatives(id, 1)
+    return relatives
+
+# Уровень родства
+def relationship_ratio(cow_id, bull_id, pedigree):
+    cow_relatives = get_relatives(pedigree, cow_id)
+    bull_relatives = get_relatives(pedigree, bull_id)
+    common_relatives = list(cow_relatives.keys() & bull_relatives.keys())
+    ratio = 0
+    for relative in common_relatives:
+        p_cow = 0.5**cow_relatives[relative]
+        p_bull = 0.5**bull_relatives[relative]
+        ratio += p_cow*p_bull
+    return ratio
--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import pandas as pd
+from helper import read_from_csv, relationship_ratio
 
 # Чтение данных
-pedigree = pd.read_csv('data/pedigree.csv', encoding='utf-8', sep=',')
-bulls = pd.read_csv('data/bulls.csv', encoding='utf-8', sep=',')
-cows = pd.read_csv('data/cows.csv', encoding='utf-8', sep=',')
+pedigree = read_from_csv('data/pedigree.csv')
+bulls = read_from_csv('data/bulls.csv')
+cows = read_from_csv('data/cows.csv')
 
-# Формирование начальной популяции
-bull_limit = cows.shape[0]*0.1
-pairs = pd.DataFrame(columns=['cow_id', 'bull_id'])
-for cow in cows.values:
-    cow_id = cow[0]
-    bull_id = bulls.sample().iloc[0]['id']
-    new_pair = pd.DataFrame({'cow_id': [cow_id], 'bull_id': [bull_id]})
-    pairs = pd.concat([pairs, new_pair], ignore_index=True)
+# print(relationship_ratio('FR00000003715', 'RU00000003716', pedigree))
